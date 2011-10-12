c1546b601f4320ac3e0ed8d8fbe8211de7af8b37
Andrew Dinn <adinn@redhat.com>
2011-10-12 12:12:28 +0100

    added check method flagging return or throw action not at end of DO list

--- org.jboss.byteman.eclipse.Byteman/src/org/jboss/byteman/eclipse/validation/BytemanJavaValidator.java
+++ org.jboss.byteman.eclipse.Byteman/src/org/jboss/byteman/eclipse/validation/BytemanJavaValidator.java
@@ -1,23 +1,44 @@
 package org.jboss.byteman.eclipse.validation;
 
+import org.eclipse.emf.common.util.EList;
 import org.eclipse.xtext.validation.Check;
 import org.jboss.byteman.eclipse.byteman.BytemanPackage;
-import org.jboss.byteman.eclipse.byteman.BytemanRule;
-import org.jboss.byteman.eclipse.byteman.EventClass;
+import org.jboss.byteman.eclipse.byteman.Actions;
+import org.jboss.byteman.eclipse.byteman.ActionList;
+import org.jboss.byteman.eclipse.byteman.Action;
+import org.jboss.byteman.eclipse.byteman.Expression;
  
 
 public class BytemanJavaValidator extends AbstractBytemanJavaValidator {
 
-//	@Check
-//	public void checkGreetingStartsWithCapital(Greeting greeting) {
-//		if (!Character.isUpperCase(greeting.getName().charAt(0))) {
-//			warning("Name should start with a capital", MyDslPackage.Literals.GREETING__NAME);
-//		}
-//	}
-
+/**
+ * quick check rule to ensure that return or throw actions only occur at
+ * the end of the action list
+ * @param actions the parse tree element representing DO  clause
+ */
 	@Check
-	public void checkBytemanClass(EventClass eventClass)
+	public void checkThrowReturnActionAtEnd(Actions actions)
 	{
-		String name = eventClass.getName();
+		ActionList actionList = actions.getActionList();
+		if (actionList != null) {
+			EList<Action> children = actionList.getActions();
+			int length = children.size();
+			for (int i = 0; i <length - 1; i++) {
+				Action action = children.get(i);
+				if (action instanceof Expression) {
+					Expression expression = (Expression) action;
+					String op = expression.getOp();
+					if (op != null) {
+						if (op.equals("return") ||
+								op.equals("RETURN")) {
+							error("RETURN expression must be last in DO actions", BytemanPackage.eINSTANCE.getActions_ActionList(), i);
+						} else if (op.equals("throw") ||
+								op.equals("THROW")) {
+							error("THROW expression must be last in DO actions", BytemanPackage.eINSTANCE.getActions_ActionList(), i);
+						}
+					}
+				}	
+			}
+		}
 	}
 }
