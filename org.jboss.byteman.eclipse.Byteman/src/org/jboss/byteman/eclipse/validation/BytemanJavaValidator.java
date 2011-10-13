6ce042d8523a2afe3373746cfcb0b409740c79d6
Andrew Dinn <adinn@redhat.com>
2011-10-13 16:18:54 +0100

    added check function to identify references to undeclared rule variables

--- org.jboss.byteman.eclipse.Byteman/src/org/jboss/byteman/eclipse/validation/BytemanJavaValidator.java
+++ org.jboss.byteman.eclipse.Byteman/src/org/jboss/byteman/eclipse/validation/BytemanJavaValidator.java
@@ -1,7 +1,10 @@
 package org.jboss.byteman.eclipse.validation;
 
 import org.eclipse.emf.common.util.EList;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.EStructuralFeature;
 import org.eclipse.xtext.validation.Check;
+import org.eclipse.xtext.validation.CheckType;
 import org.jboss.byteman.eclipse.byteman.AccessExpr;
 import org.jboss.byteman.eclipse.byteman.BytemanPackage;
 import org.jboss.byteman.eclipse.byteman.Actions;
@@ -12,6 +15,7 @@ import org.jboss.byteman.eclipse.byteman.AssignmentExpr;
 import org.jboss.byteman.eclipse.byteman.IndexedSpecialVar;
 import org.jboss.byteman.eclipse.byteman.SimpleName;
 import org.jboss.byteman.eclipse.byteman.SpecialVar;
+import org.jboss.byteman.eclipse.byteman.Binding;
  
 
 public class BytemanJavaValidator extends AbstractBytemanJavaValidator {
@@ -85,6 +89,88 @@ public class BytemanJavaValidator extends AbstractBytemanJavaValidator {
 		} else {
 			// any other LHS expression is invalid 
 			error("Invalid target for assignment operation", BytemanPackage.eINSTANCE.getExpression_Operand1());
+		}
+	}
+	
+	/**
+	 * quick check which stashes away details of the name and type of all bound variables
+	 * so we can validate susbequent references to them 
+	 * @param binding
+	 */
+	@Check
+	public void recordLocalNameIsBound(Binding binding)
+	{
+		String name = binding.getBindVariable();
+		
+		Binding oldBinding = (Binding)getContext().put(new BoundName(name), binding);
+		
+		if (oldBinding != null) {
+			error("Duplicate binding for rule variable " + name, BytemanPackage.eINSTANCE.getBinding_BindVariable());
+		}
+	}
+	
+	/**
+	 * quick check which ensures that simple names are bound before they are used
+	 * within an expression
+	 * @param expr
+	 */
+	@Check
+	public void checkLocalNameIsBound(SimpleName simpleName)
+	{
+		String name = simpleName.getValue();
+		Binding binding = (Binding) getContext().get(new BoundName(name));
+		if (binding == null) {
+			// hmm, this name may be part of a qualified name idenitfying a static
+			// field or method in which case we cannot actually check this until
+			// we are in a position to do typechecking. in any other case though
+			// we can flag an error
+			
+			// so we need to detect a situation where this simple name is the
+			// operand1 of a member access expression i.e. the containing
+			// expression is  a term of the form simpleName "." xxx
+			// in that case this 
+			EStructuralFeature feature = simpleName.eContainingFeature();
+			EObject container = simpleName.eContainer();
+			boolean okSoFar = (container instanceof AccessExpr);
+			if (okSoFar) {
+				// ok, so its an access expression but let's check it is a
+				// member expresson
+				AccessExpr accessExpr = (AccessExpr)container;
+				okSoFar = accessExpr.getOp().equals(".");
+				if (okSoFar) {
+					okSoFar = (feature == BytemanPackage.eINSTANCE.getExpression_Operand1());
+				}
+			}
+			
+		    if (!okSoFar) {
+				error("Unknown rule variable " + name, BytemanPackage.eINSTANCE.getSimpleName_Value());
+			}
+		}
+	}
+	
+	// wrapper class used to store bindings for names in
+	private static class BoundName
+	{
+		private String name;
+		BoundName(String name)
+		{
+			this.name = name;
+		}
+		
+		@Override
+		public boolean equals(Object object)
+		{
+			if (object instanceof BoundName) {
+				return ((BoundName)object).name.equals(name);
+			}
+			
+			return false;
+		}
+		
+		@Override
+		public int hashCode()
+		{
+			return name.hashCode();
 		}
 	}
 }
