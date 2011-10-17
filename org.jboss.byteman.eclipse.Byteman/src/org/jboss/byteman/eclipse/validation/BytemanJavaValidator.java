37e947531d4726a7b50bb207b460013807adc655
Andrew Dinn <adinn@redhat.com>
2011-10-17 16:43:47 +0100

    added check method which searches Java workspace to validate correctness of names supplied in CLASS/INTERFACE clause and METHOD clause

--- org.jboss.byteman.eclipse.Byteman/src/org/jboss/byteman/eclipse/validation/BytemanJavaValidator.java
+++ org.jboss.byteman.eclipse.Byteman/src/org/jboss/byteman/eclipse/validation/BytemanJavaValidator.java
@@ -1,18 +1,37 @@
 package org.jboss.byteman.eclipse.validation;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.eclipse.emf.common.util.EList;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.EStructuralFeature;
 import org.eclipse.xtext.validation.Check;
 import org.eclipse.xtext.validation.CheckType;
+
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.jdt.core.JavaModelException;
+import org.eclipse.jdt.core.search.IJavaSearchConstants;
+import org.eclipse.jdt.core.search.IJavaSearchScope;
+import org.eclipse.jdt.core.search.SearchEngine;
+import org.eclipse.jdt.core.search.SearchMatch;
+import org.eclipse.jdt.core.search.SearchParticipant;
+import org.eclipse.jdt.core.search.SearchPattern;
+import org.eclipse.jdt.core.search.SearchRequestor;
+import org.eclipse.jdt.core.search.TypeNameRequestor;
+
 import org.jboss.byteman.eclipse.byteman.AccessExpr;
 import org.jboss.byteman.eclipse.byteman.BytemanPackage;
 import org.jboss.byteman.eclipse.byteman.Actions;
 import org.jboss.byteman.eclipse.byteman.ActionList;
 import org.jboss.byteman.eclipse.byteman.Action;
+import org.jboss.byteman.eclipse.byteman.EventClass;
+import org.jboss.byteman.eclipse.byteman.EventMethod;
 import org.jboss.byteman.eclipse.byteman.Expression;
 import org.jboss.byteman.eclipse.byteman.AssignmentExpr;
 import org.jboss.byteman.eclipse.byteman.IndexedSpecialVar;
+import org.jboss.byteman.eclipse.byteman.ParameterTypes;
 import org.jboss.byteman.eclipse.byteman.SimpleName;
 import org.jboss.byteman.eclipse.byteman.SpecialVar;
 import org.jboss.byteman.eclipse.byteman.Binding;
@@ -101,8 +120,9 @@ public class BytemanJavaValidator extends AbstractBytemanJavaValidator {
 	public void recordLocalNameIsBound(Binding binding)
 	{
 		String name = binding.getBindVariable();
+		String typeName = binding.getTypename();
 		
-		Binding oldBinding = (Binding)getContext().put(new BoundName(name), binding);
+		Binding oldBinding = (Binding)getContext().put(new ContextKey(name), binding);
 		
 		if (oldBinding != null) {
 			error("Duplicate binding for rule variable " + name, BytemanPackage.eINSTANCE.getBinding_BindVariable());
@@ -118,7 +138,7 @@ public class BytemanJavaValidator extends AbstractBytemanJavaValidator {
 	public void checkLocalNameIsBound(SimpleName simpleName)
 	{
 		String name = simpleName.getValue();
-		Binding binding = (Binding) getContext().get(new BoundName(name));
+		Binding binding = (Binding) getContext().get(new ContextKey(name));
 		if (binding == null) {
 			// hmm, this name may be part of a qualified name idenitfying a static
 			// field or method in which case we cannot actually check this until
@@ -148,11 +168,262 @@ public class BytemanJavaValidator extends AbstractBytemanJavaValidator {
 		}
 	}
 	
-	// wrapper class used to store bindings for names in
-	private static class BoundName
+	/**
+	 * quick check which ensures that the target class or interface is known in the workspace 
+	 * @param eventClass
+	 * 
+	 * TODO -- decide whether we need to make this check NORMAL rather than FAST
+	 * TODO -- n.b. that will also require making other dependent type checks NORMAL
+	 */
+	@Check(CheckType.FAST)
+	public void checkClassClause(EventClass eventClass)
+	{
+		boolean isInterface = eventClass.getKeyword().equalsIgnoreCase("INTERFACE");
+		String fullName = eventClass.getName();
+		String cachedResultKey = (isInterface ? "interface " : "class ") + fullName;
+		List<TypeSearchResult> cachedResults =(List<TypeSearchResult>)getContext().get(cachedResultKey);
+		if (cachedResults == null) {
+			String typeName = fullName;
+			String packageName = null;
+			int dotIndex =typeName.lastIndexOf('.');
+			int len = typeName.length();
+			
+			if (dotIndex > 0 && dotIndex < len -1) {
+				packageName = typeName.substring(0, dotIndex);
+				typeName = typeName.substring(dotIndex + 1);
+			}
+
+			SearchEngine searchEngine = new SearchEngine();
+			final List<TypeSearchResult> results = new ArrayList<TypeSearchResult>();
+			TypeNameRequestor requestor = new TypeNameRequestor() {
+				public void acceptType(int modifiers, char[] packageName, char[] simpleTypeName, char[][] enclosingTypeNames, String path) {
+					results.add(new TypeSearchResult(modifiers, packageName, simpleTypeName, enclosingTypeNames, path));
+				}
+			};
+			char[] packageNameChars = (packageName == null ? null : packageName.toCharArray());
+			char[] typeNameChars = typeName.toCharArray();
+			
+			IJavaSearchScope searchScope = SearchEngine.createWorkspaceScope();
+
+			try {
+				searchEngine.searchAllTypeNames(packageNameChars,
+						SearchPattern.R_EXACT_MATCH|SearchPattern.R_CASE_SENSITIVE,
+						typeNameChars,
+						SearchPattern.R_EXACT_MATCH|SearchPattern.R_CASE_SENSITIVE,
+						(isInterface ? IJavaSearchConstants.INTERFACE : IJavaSearchConstants.CLASS),
+						searchScope,
+						requestor,
+						IJavaSearchConstants.WAIT_UNTIL_READY_TO_SEARCH,
+						null);
+			} catch (JavaModelException e) {
+				// TODO : ho hum probably should not happen but just ignore for now
+			}
+
+			// if we have any matches then we are ok otherwise we flag the classname
+			// as invalid
+			
+			if (results.isEmpty()) {
+				if (isInterface) {
+					error("unknown trigger interface " + fullName, BytemanPackage.eINSTANCE.getEventClass_Name());
+				} else {
+					error("unknown trigger class " + fullName, BytemanPackage.eINSTANCE.getEventClass_Name());
+				}
+				// erase any current rule class
+				getContext().remove(RULE_CLASS_KEY);
+			} else {
+				// save this result as the current rule class and cache it
+				// so we don't have to repeat the lookup
+				getContext().put(RULE_CLASS_KEY, results);
+				getContext().put(cachedResultKey, results);
+}	
+		} else {
+			// install this result as the current rule class
+			getContext().put(RULE_CLASS_KEY, cachedResults);
+		}
+	}
+
+	@Check(CheckType.FAST)
+	public void checkMethodClause(EventMethod eventMethod)
+	{
+		// delete any previous cache of possible trigger methods
+		getContext().remove(RULE_METHOD_KEY);
+
+		String methodName = eventMethod.getName();
+		// we can only check the method if we already know the possible classes
+		List<TypeSearchResult> types = (List<TypeSearchResult>)getContext().get(RULE_CLASS_KEY);
+		if (types == null) {
+			error("unknown trigger method " + methodName, BytemanPackage.eINSTANCE.getEventMethod_Name());
+		} else if (methodName.equals("<clinit>")) {
+			// cannot verify presence of <clinit> via search
+			// just check it has no arguments
+			ParameterTypes parameterTypes = eventMethod.getParameterTypes();
+			if (parameterTypes != null) {
+				EList<String> paramTypeNames = parameterTypes.getParamTypeNames();
+				int paramTypeCount = paramTypeNames.size();
+				
+				if (paramTypeCount != 0) {
+					error("invalid parameter types for class initializer" + methodName, BytemanPackage.eINSTANCE.getEventMethod_ParameterTypes());
+				}
+			}
+		} else {
+			boolean isConstructor = methodName.equals("<init>");
+			// look for methods on each of the possible types
+			ParameterTypes parameterTypes = eventMethod.getParameterTypes();
+			EList<String> paramTypeNames;
+			int paramTypeCount;
+			if (parameterTypes != null) {
+				paramTypeNames = parameterTypes.getParamTypeNames();
+				paramTypeCount = paramTypeNames.size();
+			} else {
+				paramTypeNames = null;
+				// -1 indicates any method with the relevant signature will do
+				// whereas 0 indicates an empty parameter list ()
+				paramTypeCount = -1;
+			}
+
+			final List<MethodSearchResult> methods = new ArrayList<MethodSearchResult>();
+			// accumulate matching methods
+			
+			SearchEngine searchEngine = new SearchEngine();
+			IJavaSearchScope scope = SearchEngine.createWorkspaceScope();			
+			SearchParticipant[] participants = new SearchParticipant[] {SearchEngine.getDefaultSearchParticipant()};
+
+			for (TypeSearchResult result : types) {
+				String typeName = result.name;
+				
+				// now build type qualified method name
+				StringBuilder builder = new StringBuilder();
+				builder.append(typeName);
+				// append method name to type name except when it is a constructor or class initializer
+				if (!isConstructor) {
+					builder.append('.');
+					builder.append(methodName);
+				}
+				if (paramTypeCount >= 0) {
+					String separator = "";
+					builder.append("(");
+					for (int i = 0; i < paramTypeCount; i++) {
+						builder.append(separator);
+						builder.append(paramTypeNames.get(i));
+						separator = ",";
+					}
+					builder.append(")");
+				}
+				final String stringPattern = builder.toString();
+				int searchFor = (isConstructor ? IJavaSearchConstants.CONSTRUCTOR : IJavaSearchConstants.METHOD);
+				int limitTo = IJavaSearchConstants.DECLARATIONS;
+				int matchType = SearchPattern.R_EXACT_MATCH|SearchPattern.R_CASE_SENSITIVE;
+				SearchPattern pattern = SearchPattern.createPattern(stringPattern, searchFor, limitTo, matchType);
+				final TypeSearchResult typeResult = result;
+				SearchRequestor requestor = new SearchRequestor() {
+					@Override
+					public void acceptSearchMatch(SearchMatch match)
+							throws CoreException {
+						// only accept if we have an accurate match
+						if (match.getAccuracy() == SearchMatch.A_ACCURATE) {
+							MethodSearchResult methodResult = new MethodSearchResult(stringPattern, typeResult, match);
+							methods.add(methodResult);
+						}
+					}				
+				};
+				try {
+					searchEngine.search(pattern, participants, scope, requestor, null);
+				} catch (CoreException e) {
+					// TODO : ho hum not sure if this will happen when we have
+					// no such method or because something is wrong with paths etc
+					// but just ignore for now
+				}
+			}
+			
+			// if we have no matches then plant an error
+			
+			if (methods.isEmpty()) {
+				error("unknown trigger method " + methodName, BytemanPackage.eINSTANCE.getEventMethod_Name());
+			} else {
+				// cache details of potential trigger methods for current rule
+				getContext().put(RULE_METHOD_KEY, methods);
+			}
+		}
+	}
+	
+	/**
+	 * 	wrapper class used to hold results of type name searches
+	 */
+	private class TypeSearchResult
+	{
+		int modifiers;
+		char[] packageName;
+		char[] simpleTypeName;
+		char[][] enclosingTypeNames;
+		String path;
+		String name;
+		TypeSearchResult(int modifiers, char[] packageName, char[] simpleTypeName, char[][] enclosingTypeNames, String path)
+		{
+			this.modifiers = modifiers;
+			this.packageName = packageName;
+			this.simpleTypeName = simpleTypeName;
+			this.enclosingTypeNames = enclosingTypeNames;
+			this.path = path;
+			StringBuilder builder = new StringBuilder();
+			if (packageName != null && packageName.length > 0) {
+				builder.append(packageName);
+				builder.append('.');
+			}
+			for (char[] enclosingTypeName : enclosingTypeNames) {
+				builder.append(enclosingTypeNames);
+				builder.append('.');
+			}
+			builder.append(simpleTypeName);		
+			name = builder.toString();
+		}
+	}
+	
+	/**
+	 * 	wrapper class used to hold results of method searches
+	 */
+	private class MethodSearchResult
+	{
+		TypeSearchResult typeSearchResult;
+		String methodSpec;
+		SearchMatch match;
+		
+		MethodSearchResult(String methodSpec, TypeSearchResult typeSearchResult, SearchMatch match)
+		{
+			this.methodSpec = methodSpec;
+			this.typeSearchResult = typeSearchResult;
+			this.match = match;
+		}
+	}
+	
+	/**
+	 * key used to identify validation context entry for the name of the current RULE
+	 * 
+	 * associated value is a String
+	 */
+	private final static ContextKey RULE_NAME_KEY =new ContextKey("RULE:");
+/**
+	 * key used to identify validation context entry for the list of possible targets
+	 * for the name supplied in the CLASS clause of the current RULE
+	 * 
+	 * associated value is a List<TypeSearchResult>
+	 */
+	private final static ContextKey RULE_CLASS_KEY =new ContextKey("CLASS:");
+	/**
+	 * key used to identify validation context entry for the list of possible targets
+	 * for the name supplied in the METHOD clause
+	 * 
+	 * associated value is a List<MethodSearchResult>
+	 */
+	private final static ContextKey RULE_METHOD_KEY = new ContextKey("METHOD:");
+
+
+	// wrapper key used to identify values stored in the context -- using this to
+	// wrap the String keys avoids us overwriting anything stored there by eclipse
+	
+	private static class ContextKey
 	{
 		private String name;
-		BoundName(String name)
+		ContextKey(String name)
 		{
 			this.name = name;
 		}
@@ -160,8 +431,8 @@ public class BytemanJavaValidator extends AbstractBytemanJavaValidator {
 		@Override
 		public boolean equals(Object object)
 		{
-			if (object instanceof BoundName) {
-				return ((BoundName)object).name.equals(name);
+			if (object instanceof ContextKey) {
+				return ((ContextKey)object).name.equals(name);
 			}
 			
 			return false;
